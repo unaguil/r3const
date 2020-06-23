fa039fea512b699fa3bc202a2ccb5115debac139
Unai Aguilera <unai.aguilera@deusto.es>
2020-06-23 11:11:50 +0200

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='r3const',
+    version='1.0.0',
+    url='https://github.com/unaguil/r3const.git',
+    author='Unai Aguilera',
+    author_email='unai.aguilera@deusto.es',
+    description='Minimal 3d scene manager',
+    packages=find_packages(),    
+    install_requires=['panda==1.10.6'],
+)
